c69cd484bcc3e6ae923f149bec23164df0491a0f
Emerson Fang <efang@seas.harvard.edu>
2015-12-13 15:19:18 -0500

    updated Users Table Seeder, caught a typo in naming convention (singular not plural)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
-        $this->call(UserTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
         $this->call(LandmarksTableSeeder::class);
         $this->call(PhotosTableSeeder::class);
         $this->call(ReviewsTableSeeder::class);
