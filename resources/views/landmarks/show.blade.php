2b1bcc48672eb923289c8f081048609d1c159644
Emerson Fang <efang@seas.harvard.edu>
2015-12-14 11:14:19 -0500

    updated show page again, made the header change based on whether or not there are review

--- resources/views/landmarks/show.blade.php
+++ resources/views/landmarks/show.blade.php
@@ -13,25 +13,36 @@
                 foreach($photos as $photo) {
                     $filepath = $photo['filepath'];
             };?>
-            <h1>Show landmark: {{ $landmark->name }}</h1>
+            <h1>Showing reviews for: <br> {{ $landmark->name }}</h1>
+                <div class="centered_text">
+                    @if(Auth::check())
+                        <a href='/reviews/create'>Add a review</a> |
+                        <a href='/photos/create'>Add a photo</a>
+                    @else
+                    @endif
+                </div>
             <div class="image">
                 <img style='width:20%' src={{$filepath}}>
             </div>
-            <h2>Reviews:</h2>
-            <div class="container">
-                <?php $reviews = \App\Review::where('landmark_id','=',$landmark->id)->orderBy('id')->get();?>
-                @foreach($reviews as $review)
-                    <?php $users= \App\User::where('id','=',$review->user_id)->get();
-                        foreach($users as $user) {
-                            $user_name = $user->name;
-                        };?>
-                    <div style="text-align: center" class="review_results_container">
-                            {{$review->review}}
-                            <br>
-                            --{{$user_name}}
-                    </div>
-                @endforeach
-            </div>
+            <?php $reviews = \App\Review::where('landmark_id','=',$landmark->id)->orderBy('id')->get();?>
+            @if(empty($reviews->first()))
+                <h2>No Reviews (Yet!)</h2>
+            @else
+                <h2>Reviews:</h2>
+                <div class="container">
+                        @foreach($reviews as $review)
+                            <?php $users= \App\User::where('id','=',$review->user_id)->get();
+                                foreach($users as $user) {
+                                    $user_name = $user->name;
+                                };?>
+                            <div style="text-align: center" class="review_results_container">
+                                    {{$review->review}}
+                                    <br>
+                                    --{{$user_name}}
+                            </div>
+                        @endforeach
+                </div>
+            @endif
 @stop
 
 
