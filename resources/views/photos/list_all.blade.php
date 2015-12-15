05d9cbd98db831630597cc936e76206134b1ff47
Emerson Fang <efang@seas.harvard.edu>
2015-12-15 00:08:59 -0500

    fiddled around with photos views to add more options to nav and to links under header for each view

--- resources/views/photos/list_all.blade.php
+++ resources/views/photos/list_all.blade.php
@@ -10,14 +10,25 @@
 
 @section('content')
     <div class="container">
-        <h1>Here are all the photos on the site for {{$landmark->name}}:</h1>
+        <h1>Here are all the photos on the site for:</h1>
+        <h2>{{$landmark->name}}</h2>
+        <div class="centered_text">
+            @if(Auth::check())
+                <a href='/photos/{{$landmark->id}}/create'>Add a photo</a>
+            @else
+            @endif
+        </div>
 
         <?php $photos = \App\Photo::where('landmark_id','=',$landmark->id)->orderBy('id')->get(); ?>
         @foreach($photos as $photo)
+            <?php
+            $user_name = DB::table('users')
+                    ->where('id', '=',$photo->user_id)->value('name');
+            ?>
             <div style="text-align: center" class="user_results_container">
                 <h2>Photo ID#: {{ $photo->id }}</h2>
-                <a href='show/{{$landmark->id}}'>Reviews</a> |
-                <a href='/photos/{{$landmark->id}}'>Photos</a>
+                <div class="centered_text">Uploaded by {{$user_name}}</div>
+                <br>
                 <div class="outer">
                     <div class="image">
                         <img style='width:100%' src={{$photo->filepath}}>
